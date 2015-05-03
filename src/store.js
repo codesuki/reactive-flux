4ec62a541640c9a8bd62ea1773df02b01c6c2eb1
Jacob Parker <jacobparker1992@gmail.com>
2015-05-03 10:56:13 +0100

    Add component mixin for stores to automate subscribing/unsubscribing
    See README.md for details on usage

--- src/store.js
+++ src/store.js
@@ -3,11 +3,22 @@ var Rx = require('rx');
 class Store extends Rx.Subject {
     constructor() {
 	super();
+
+	var self = this;
+	this.mixin = {
+		onSubscribe() {}, // Override me
+	    componentDidMount() {
+	       this._subscription = self.subscribe(this.onSubscribe)
+	    },
+	    componentWillUnmount() {
+	       this._subscription = self.dispose()
+	    }
+	};
     }
 
     observe(action, observer) {
 	var self = this;
-	
+
 	return action.subscribe((data) => {
 	    observer.call(self, data);
 	    this.onNext();
